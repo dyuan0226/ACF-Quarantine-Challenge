bbedf37e6e3c7f74cf03714716b5b85647c1a8ce
rickyma9 <48923521+rickyma9@users.noreply.github.com>
2020-06-01 18:04:24 -0400

    added some sets for users and teams, added teams tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  # PROPOSED CHANGES TO THE SPECS:
+  # 1. Users -> teams should be 0-many beause new users will not have a team yet (allow_blank for team_id should be set to true)
+  # 2. only active users can be in teams
 end
--- test/sets/users.rb
+++ test/sets/users.rb
@@ -1,9 +1,19 @@
 module Contexts
   module Users
     def create_users
+      @david_top_team = FactoryBot.create(:user, first_name: "David", last_name: "Yuan", team: @top_team_active, username: "dyuan", role: "admin", email: "dyuan@gmail.com", active: true)
+      @amy_top_team = FactoryBot.create(:user, first_name: "Amu", last_name: "Lu", team: @top_team_active, username: "alu", role: "admin", email: "alu@gmail.com", active: true)
+      @ricky_bottom_team = FactoryBot.create(:user, first_name: "Ricky", last_name: "Ma", team: @bottom_team_active, username: "rma", role: "regular", email: "rma@gmail.com", active: true)
+      @matt_bottom_team = FactoryBot.create(:user, first_name: "Matthew", last_name: "Fang", team: @bottom_team_active, username: "mfang", role: "regular", email: "mfang@gmail.com", active: true)
+      @kimberly_inactive_team = FactoryBot.create(:user, first_name: "Kimberly", last_name: "Lo", team: @inactive_team, username: "klo", role: "regular", email: "klo@gmail.com", active: true)
     end
     
     def destroy_users
+      @david_top_team.destroy
+      @amy_top_team.destroy
+      @ricky_bottom_team.destroy
+      @matt_bottom_team.destroy
+      @kimberly_inactive_team.destroy
     end
   end
 end
