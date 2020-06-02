6c95041d08f697eb496248de822c199f3021c6b8
David Yuan <dyuan1@andrew.cmu.edu>
2020-06-01 23:58:09 -0400

    wrote challenge and submission tests

--- test/sets/submissions.rb
+++ test/sets/submissions.rb
@@ -5,7 +5,7 @@ module Contexts
       @sub2 = FactoryBot.create(:submission, challenge: @sleep_well, user: @david_top_team, date_completed: 2.days.ago.to_date)
       @sub3 = FactoryBot.create(:submission, challenge: @write_poetry, user: @david_top_team, date_completed: 3.days.ago.to_date)
       @sub4 = FactoryBot.create(:submission, challenge: @read_john, user: @amy_top_team, date_completed: 4.days.ago.to_date)
-      @sub5 = FactoryBot.create(:submission, challenge: @chore_tracker, user: @amy_top_team, date_completed: 2.days.ago.to_date)
+      @sub5 = FactoryBot.create(:submission, challenge: @sleep_well, user: @amy_top_team, date_completed: 2.days.ago.to_date)
       @sub6 = FactoryBot.create(:submission, challenge: @read_john, user: @ricky_bottom_team, date_completed: 2.days.ago.to_date)
       @sub7 = FactoryBot.create(:submission, challenge: @memorize_2_tim, user: @ricky_bottom_team)
       @sub8 = FactoryBot.create(:submission, challenge: @write_poetry, user: @ricky_bottom_team, date_completed: 2.days.ago.to_date)
