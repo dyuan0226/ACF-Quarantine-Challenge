1f02dc6568585c86cdb2dfb538aba4a67d6e2d6a
David Yuan <dyuan1@andrew.cmu.edu>
2020-06-05 17:05:17 -0400

    wrote half of challenge model

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -4,10 +4,13 @@ class Challenge < ApplicationRecord
   has_many :users, through: :submissions
 
   # Validations
-
+  validates_presence_of :name, :num_points, :category
+  validates_numericality_of :num_points, greater_than: 0, only_integer: true
+  validates_inclusion_of :category, in:  %w[Bible Lifestyle Creative]
 
   # Scopes
-
+  scope :alphabetical,  -> { order('name') }
+  scope :for_category,  ->(category) { where("category = ?", category) }
 
   # Callbacks
 
--- test/models/challenge_test.rb
+++ test/models/challenge_test.rb
@@ -22,62 +22,62 @@ class ChallengeTest < ActiveSupport::TestCase
   should_not allow_value(3.5).for(:num_points)
   should_not allow_value(-7.8).for(:num_points)
 
-  # For use when we come up with the categories
-  # should allow_value("First category").for(:category)
-  # should allow_value("Second category").for(:category)
-  # should allow_value("Third category").for(:category)
-  # should allow_value("Fourth category").for(:category)
-  # should_not allow_value("Anything else").for(:category)
+  # Modify when we come up with the categories
+  should allow_value("Bible").for(:category)
+  should allow_value("Lifestyle").for(:category)
+  should allow_value("Creative").for(:category)
+  should_not allow_value("Anything else").for(:category)
+  should_not allow_value(5).for(:category)
 
   context "Given context" do
     setup do
-      create_teams
-      create_users
+      # create_teams
+      # create_users
       create_challenges
-      create_submissions
+      # create_submissions
     end
 
     teardown do
-      destroy_submissions
+      # destroy_submissions
       destroy_challenges
-      destroy_users
-      destroy_teams
+      # destroy_users
+      # destroy_teams
     end
 
-    should "not be deleted if it has a submission" do
-      @chore_tracker.destroy
-      assert @chore_tracker.destroyed?
+    # should "not be deleted if it has a submission" do
+    #   @chore_tracker.destroy
+    #   assert @chore_tracker.destroyed?
 
-      @read_john.destroy
-      deny @read_john.destroyed?
-      @write_poetry.destroy
-      deny @write_poetry.destroyed?
-    end
+    #   @read_john.destroy
+    #   deny @read_john.destroyed?
+    #   @write_poetry.destroy
+    #   deny @write_poetry.destroyed?
+    # end
 
     should "list challenges alphabetically" do
       assert_equal [@chore_tracker, @memorize_2_tim, @read_john, @sleep_well, @write_poetry], Challenge.alphabetical
     end
 
-    should "have a scope 'for_user_completed' that works" do
-      assert_equal 3, Challenge.for_user_completed(@david_top_team).size
-      assert_equal [@read_john, @sleep_well, @write_poetry], Challenge.for_user_completed(@david_top_team).alphabetical
+    # should "have a scope 'for_user_completed' that works" do
+    #   assert_equal 3, Challenge.for_user_completed(@david_top_team).size
+    #   assert_equal [@read_john, @sleep_well, @write_poetry], Challenge.for_user_completed(@david_top_team).alphabetical
 
-      assert_equal 2, Challenge.for_user_completed(@amy_top_team).size
-      assert_equal [@read_john, @sleep_well], Challenge.for_user_completed(@amy_top_team).alphabetical
+    #   assert_equal 2, Challenge.for_user_completed(@amy_top_team).size
+    #   assert_equal [@read_john, @sleep_well], Challenge.for_user_completed(@amy_top_team).alphabetical
 
-      assert_equal 0, Challenge.for_user_completed(@matt_bottom_team).size
-    end
+    #   assert_equal 0, Challenge.for_user_completed(@matt_bottom_team).size
+    # end
 
-    should "have a scope 'for_user_incomplete' that works" do
-      assert_equal 2, Challenge.for_user_incomplete(@david_top_team).size
-      assert_equal [@chore_tracker, @memorize_2_tim], Challenge.for_user_incomplete(@david_top_team).alphabetical
+    # should "have a scope 'for_user_incomplete' that works" do
+    #   assert_equal 2, Challenge.for_user_incomplete(@david_top_team).size
+    #   assert_equal [@chore_tracker, @memorize_2_tim], Challenge.for_user_incomplete(@david_top_team).alphabetical
 
-      assert_equal 3, Challenge.for_user_incomplete(@amy_top_team).size
-      assert_equal [@chore_tracker, @memorize_2_tim, @write_poetry], Challenge.for_user_incomplete(@amy_top_team).alphabetical
+    #   assert_equal 3, Challenge.for_user_incomplete(@amy_top_team).size
+    #   assert_equal [@chore_tracker, @memorize_2_tim, @write_poetry], Challenge.for_user_incomplete(@amy_top_team).alphabetical
 
-      assert_equal 5, Challenge.for_user_incomplete(@matt_bottom_team).size
-      assert_equal Challenge.alphabetical, Challenge.for_user_incomplete(@matt_bottom_team).alphabetical
-    end
+    #   assert_equal 5, Challenge.for_user_incomplete(@matt_bottom_team).size
+    #   assert_equal Challenge.alphabetical, Challenge.for_user_incomplete(@matt_bottom_team).alphabetical
+    # end
 
     should "have a scope 'for_category' that works" do
       assert_equal 2, Challenge.for_category("Bible").size
