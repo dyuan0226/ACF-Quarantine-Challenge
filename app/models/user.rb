8ceaed834ad3a2eaaca0d46b8e76e324db5b6aad
David Yuan <dyuan1@andrew.cmu.edu>
2020-06-07 21:41:25 -0400

    wrote part of user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,16 @@ class User < ApplicationRecord
 
   # Validations
   validates_presence_of :first_name, :last_name, :username, :email
-  # validates_format_of :email, with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  # validates_uniqueness_of :username, case_sensitive: false
-  # validates_presence_of :password, :on => :create 
-  # validates_presence_of :password_confirmation, :on => :create 
-  # validates_confirmation_of :password, message: "does not match"
-  # validates_length_of :password, :minimum => 4, message: "must be at least 4 characters long", :allow_blank => true
-  # validates_inclusion_of :role, in: %w[admin regular], message: "is not recognized in the system"
+  validates_format_of :email, with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates_format_of :first_name, with: /\A([A-Z])[a-z]+\z/, message: "First name should be capitalized!"
+  validates_format_of :last_name, with: /\A([A-Z])[a-z]+\z/, message: "Last name should be capitalized!"
+  validates_uniqueness_of :username, case_sensitive: false
+  validates_uniqueness_of :email, case_sensitive: false
+  validates_presence_of :password, :on => :create 
+  validates_presence_of :password_confirmation, :on => :create 
+  validates_confirmation_of :password, message: "does not match"
+  validates_length_of :password, :minimum => 4, message: "must be at least 4 characters long", :allow_blank => true
+  validates_inclusion_of :role, in: %w[admin regular], message: "is not recognized in the system"
 
   # Scopes
 
@@ -25,7 +28,9 @@ class User < ApplicationRecord
 
 
   # Methods
-  
+  def self.authenticate(username, password)
+    find_by_username(username).try(:authenticate, password)
+  end
 
   private
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -18,137 +18,151 @@ class UserTest < ActiveSupport::TestCase
   should validate_presence_of(:first_name)
   should validate_presence_of(:last_name)
   should validate_presence_of(:username)
-  # should validate_presence_of(:password)
-  # should validate_presence_of(:password_confirmation)
+  should validate_presence_of(:password)
+  should validate_presence_of(:password_confirmation)
   should validate_presence_of(:email)
+  should validate_uniqueness_of(:username).case_insensitive()
+  should validate_uniqueness_of(:email).case_insensitive()
   should have_secure_password
 
-  # should allow_value("asdf").for(:first_name)
-  # should allow_value("Ricky").for(:first_name)
-  # should allow_value("David").for(:first_name)
-  # should_not allow_value(1234).for(:first_name)
-  # should_not allow_value(1.2).for(:first_name)
-  # should_not allow_value(true).for(:first_name)
-  # should_not allow_value(nil).for(:first_name)
+  should allow_value("Ricky").for(:first_name)
+  should allow_value("David").for(:first_name)
+  should_not allow_value("asdf").for(:first_name)
+  should_not allow_value(1234).for(:first_name)
+  should_not allow_value(1.2).for(:first_name)
+  should_not allow_value(true).for(:first_name)
+  should_not allow_value(nil).for(:first_name)
 
-  # should allow_value("asdf").for(:last_name)
-  # should allow_value("Ricky").for(:last_name)
-  # should allow_value("David").for(:last_name)
-  # should_not allow_value(1234).for(:last_name)
-  # should_not allow_value(1.2).for(:last_name)
-  # should_not allow_value(true).for(:last_name)
-  # should_not allow_value(nil).for(:last_name)
+  should allow_value("Ricky").for(:last_name)
+  should allow_value("David").for(:last_name)
+  should_not allow_value("asdf").for(:last_name)
+  should_not allow_value(1234).for(:last_name)
+  should_not allow_value(1.2).for(:last_name)
+  should_not allow_value(true).for(:last_name)
+  should_not allow_value(nil).for(:last_name)
 
-  # should allow_value("kjdslsfdsjlakjs").for(:username)
-  # should allow_value("rma1").for(:username)
-  # should allow_value("dyuan20").for(:username)
-  # should_not allow_value(1234).for(:username)
-  # should_not allow_value(1.2).for(:username)
-  # should_not allow_value(true).for(:username)
-  # should_not allow_value(nil).for(:username)
+  should allow_value("kjdslsfdsjlakjs").for(:username)
+  should allow_value("rma1").for(:username)
+  should allow_value("dyuan20").for(:username)
 
-  # should allow_value("admin").for(:role)
-  # should allow_value("regular").for(:role)
-  # should_not allow_value(1234).for(:role)
-  # should_not allow_value(1.2).for(:role)
-  # should_not allow_value(true).for(:role)
-  # should_not allow_value("anything else").for(:role)
+  should allow_value("admin").for(:role)
+  should allow_value("regular").for(:role)
+  should_not allow_value(1234).for(:role)
+  should_not allow_value(1.2).for(:role)
+  should_not allow_value(true).for(:role)
+  should_not allow_value("anything else").for(:role)
 
-  # should allow_value("ricky-ma-29@gmail.com").for(:email)
-  # should allow_value("david_yuan_09@gmail.com").for(:email)
-  # should_not allow_value("dyuan20").for(:email)
-  # should_not allow_value(1234).for(:email)
-  # should_not allow_value(1.2).for(:email)
-  # should_not allow_value(true).for(:email)
-  # should_not allow_value(nil).for(:email)
+  should allow_value("ricky-ma-29@gmail.com").for(:email)
+  should allow_value("david_yuan_09@gmail.com").for(:email)
+  should_not allow_value("dyuan20").for(:email)
+  should_not allow_value(1234).for(:email)
+  should_not allow_value(1.2).for(:email)
+  should_not allow_value(true).for(:email)
+  should_not allow_value(nil).for(:email)
 
-  # context "Given context" do 
-  #   setup do 
-  #     create_teams
-  #     create_users
-  #   end 
+  context "Given context" do 
+    setup do 
+      create_teams
+      create_users
+    end 
 
-  #   teardown do 
-  #     destroy_users
-  #     destroy_teams
-  #   end
+    teardown do 
+      destroy_users
+      destroy_teams
+    end
 
-  #   should "validate the uniqueness of usernames" do 
-  #     repeat_uname = FactoryBot.build(:user, first_name: "Imposter", last_name: "Yuan", team: @top_team_active, username: "dyuan", role: "admin", email: "dyuan20@gmail.com", active: true)
-  #     deny repeat_uname.valid?
-  #   end
+    should "do nothing" do
+      
+    end
 
-  #   should "validate the uniqueness of emails" do 
-  #     repeat_email = FactoryBot.build(:user, first_name: "Imposter", last_name: "Yuan", team: @top_team_active, username: "dyuan20", role: "admin", email: "dyuan@gmail.com", active: true)
-  #     deny repeat_email.valid?
-  #   end
+    # should "require a password for new users" do
+    #   bad_user = FactoryBot.build(:user, username: "tank", password: nil)
+    #   deny bad_user.valid?
+    # end
+    
+    # should "require passwords to be confirmed and matching" do
+    #   bad_user_1 = FactoryBot.build(:user, username: "tank", password: "secret", password_confirmation: nil)
+    #   deny bad_user_1.valid?
+    #   bad_user_2 = FactoryBot.build(:user, username: "tank", password: "secret", password_confirmation: "sauce")
+    #   deny bad_user_2.valid?
+    # end
+    
+    # should "require passwords to be at least four characters" do
+    #   bad_user = FactoryBot.build(:user, username: "tank", password: "no", password_confirmation: "no")
+    #   deny bad_user.valid?
+    # end
 
-  #   should "automatically set role to regular" do 
-  #     unfilled_role = FactoryBot.create(:user, first_name: "Imposter", last_name: "Ma", team: @top_team_active, username: "rma1", email: "rma1@gmail.com", active: true)
-  #     assert_equal "regular", unfilled_role.role 
-  #   end
+    # should "have class method to handle authentication services" do
+    #   assert User.authenticate('ed', 'secret')
+    #   deny User.authenticate('ed', 'notsecret')
+    # end
 
-  #   should "not be able to destroy a user, only set to inactive" do   # i'm not too sure if this would work due to saving and reloading to the database or whatnot
-  #     @imposter = FactoryBot.create(:user, first_name: "Imposter", last_name: "Ma", team: @top_team_active, username: "rma1", email: "rma1@gmail.com", active: true)
-  #     assert @imposter.active 
-  #     deny @imposter.destroy 
-  #     deny @imposter.active
-  #   end
+    # should "automatically set role to regular" do 
+    #   unfilled_role = FactoryBot.create(:user, first_name: "Imposter", last_name: "Ma", team: @top_team_active, username: "rma1", email: "rma1@gmail.com", active: true)
+    #   assert_equal "regular", unfilled_role.role 
+    # end
 
-  #   should "have a method to calculate the number of points of a user" do 
-  #     assert_equal 3, @amy_top_team.points
-  #     assert_equal 2, @david_top_team.points
-  #     assert_equal 1, @matt_bottom_team.points 
-  #     assert_equal 0, @inactive_user_1.points 
-  #   end
+    # should "not be able to destroy a user, only set to inactive" do   # i'm not too sure if this would work due to saving and reloading to the database or whatnot
+    #   @imposter = FactoryBot.create(:user, first_name: "Imposter", last_name: "Ma", team: @top_team_active, username: "rma1", email: "rma1@gmail.com", active: true)
+    #   assert @imposter.active 
+    #   deny @imposter.destroy 
+    #   deny @imposter.active
+    # end
 
-  #   should "have a method that returns a list of all challenges completed" do 
-  #     assert_equal [@sleep_well, @write_poetry], @david_top_team.challenges_completed
-  #     assert_equal [@read_john, @sleep_well], @amy_top_team.challenges_completed
-  #   end
+    # should "have a method to calculate the number of points of a user" do 
+    #   assert_equal 3, @amy_top_team.points
+    #   assert_equal 2, @david_top_team.points
+    #   assert_equal 1, @matt_bottom_team.points 
+    #   assert_equal 0, @inactive_user_1.points 
+    # end
 
-  #   should "have a method to make active" do 
-  #     deny @inactive_user_1.active 
-  #     @inactive_user_1.make_inactive
-  #     assert @inactive_user_1.active 
-  #   end
+    # should "have a method that returns a list of all challenges completed" do 
+    #   assert_equal [@sleep_well, @write_poetry], @david_top_team.challenges_completed
+    #   assert_equal [@read_john, @sleep_well], @amy_top_team.challenges_completed
+    # end
 
-  #   should "have a method to make inactive" do 
-  #     assert @inactive_user_1.active 
-  #     @inactive_user_1.make_inactive
-  #     deny @inactive_user_1.active 
-  #   end
+    # should "have a method to make active" do 
+    #   deny @inactive_user_1.active 
+    #   @inactive_user_1.make_inactive
+    #   assert @inactive_user_1.active 
+    # end
 
-  #   should "have a scope that returns all the users of a given team" do 
-  #     assert_equal ["Amy", "David"], User.for_team(@top_team_active).map{|u| u.first_name}.sort
-  #     assert_equal ["Matthew", "Ricky"], User.for_team(@bottom_team_active).map{|u| u.first_name}.sort
-  #   end
+    # should "have a method to make inactive" do 
+    #   assert @inactive_user_1.active 
+    #   @inactive_user_1.make_inactive
+    #   deny @inactive_user_1.active 
+    # end
 
-  #   should "have a scope that returns all the users who have completed a given challenge" do 
-  #     assert_equal ["Amy", "David", "Ricky"], User.for_challenge(@read_john).map{|u| u.first_name}.sort
-  #     assert_equal ["David", "Ricky"], User.for_challenge(@write_poetry).map{|u| u.first_name}.sort
-  #   end
+    # should "have a scope that returns all the users of a given team" do 
+    #   assert_equal ["Amy", "David"], User.for_team(@top_team_active).map{|u| u.first_name}.sort
+    #   assert_equal ["Matthew", "Ricky"], User.for_team(@bottom_team_active).map{|u| u.first_name}.sort
+    # end
 
-  #   should "have a scope that returns all the users who match a given role" do 
-  #     assert_equal ["Amy", "David"], User.for_role("admin").map{|u| u.first_name}.sort
-  #     assert_equal ["David", "inactive", "inactive", "Ricky"], User.for_role("regular").map{|u| u.first_name}.sort
-  #   end
+    # should "have a scope that returns all the users who have completed a given challenge" do 
+    #   assert_equal ["Amy", "David", "Ricky"], User.for_challenge(@read_john).map{|u| u.first_name}.sort
+    #   assert_equal ["David", "Ricky"], User.for_challenge(@write_poetry).map{|u| u.first_name}.sort
+    # end
 
-  #   should "have a scope that sorts all users by last name" do 
-  #     assert_equal ["Fang", "Lu", "Ma", "One", "Two", "Yuan"], User.by_last_name.map{|u| u.last_name}
-  #   end
+    # should "have a scope that returns all the users who match a given role" do 
+    #   assert_equal ["Amy", "David"], User.for_role("admin").map{|u| u.first_name}.sort
+    #   assert_equal ["David", "inactive", "inactive", "Ricky"], User.for_role("regular").map{|u| u.first_name}.sort
+    # end
 
-  #   should "have a scope that sorts all users by last name" do 
-  #     assert_equal ["Amy", "David", "Inactive", "Inactive", "Matthew", "Ricky"], User.by_first_name.map{|u| u.first_name}
-  #   end
+    # should "have a scope that sorts all users by last name" do 
+    #   assert_equal ["Fang", "Lu", "Ma", "One", "Two", "Yuan"], User.by_last_name.map{|u| u.last_name}
+    # end
 
-  #   should "have a scope that returns all active users" do 
-  #     assert_equal ["Amy", "David", "Matthew", "Ricky"], User.active.map{|u| u.first_name}.sort
-  #   end
+    # should "have a scope that sorts all users by last name" do 
+    #   assert_equal ["Amy", "David", "Inactive", "Inactive", "Matthew", "Ricky"], User.by_first_name.map{|u| u.first_name}
+    # end
 
-  #   should "have a scope that returns all inactive users" do 
-  #     assert_equal ["Inactive", "Inactive"], User.inactive.map{|u| u.first_name}.sort
-  #   end
+    # should "have a scope that returns all active users" do 
+    #   assert_equal ["Amy", "David", "Matthew", "Ricky"], User.active.map{|u| u.first_name}.sort
+    # end
 
-  # end
+    # should "have a scope that returns all inactive users" do 
+    #   assert_equal ["Inactive", "Inactive"], User.inactive.map{|u| u.first_name}.sort
+    # end
+
+  end
 end
