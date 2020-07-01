621b07f9d333e1e64732f9a4e8722d6a89f5f878
David Yuan <dyuan1@andrew.cmu.edu>
2020-06-30 23:26:49 -0400

    rewrote ability.rb model

---
+++ app/models/ability.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    # set user to new User if not logged in
+    user ||= User.new # i.e., a guest user
+    
+    # set authorizations for different user roles
+    if user.role? :admin
+      # they get to do it all
+      can :manage, :all
+      
+    elsif user.role? :regular
+      # they can read other user's info
+      can :index, User
+      can :show, User
+
+      can :index, Challenge
+      can :show, Challenge 
+
+      can :index, Team
+      can :show, Team
+      
+      # can :index, Photos
+
+      # they can make their own submissions
+      can :create, Submission
+
+      # they can delete themselves (make inactive)
+      can :destroy, User do |this_user|  
+        user.id == this_user.employee_id
+      end
+
+      # they can edit their own data
+      can :edit, User do |this_user|  
+        user.id == this_user.id
+      end
+
+      can :update, User do |this_user|  
+        user.id == this_user.id
+      end
+      
+    else
+      # guests can only read some other stuff?
+    end
+  end
+end
