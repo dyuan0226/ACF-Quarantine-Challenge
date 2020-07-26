c33d7a0e0e267bd32e3fc1da1f46f6a6739d9464
Ricky Ma <rickyma@andrew.cmu.edu>
2020-07-26 13:33:28 -0700

    quick edits

--- test/controllers/challenges_controller_test.rb
+++ test/controllers/challenges_controller_test.rb
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-require "test_helper"
-
-describe ChallengesController do
-  let(:challenge) { challenges :one }
-
-  it "gets index" do
-    get challenges_url
-    value(response).must_be :success?
-  end
-
-  it "gets new" do
-    get new_challenge_url
-    value(response).must_be :success?
-  end
-
-  it "creates challenge" do
-    expect {
-      post challenges_url, params: { challenge: { category: challenge.category, description: challenge.description, name: challenge.name, num_points: challenge.num_points } }
-    }.must_change "Challenge.count"
-
-    must_redirect_to challenge_path(Challenge.last)
-  end
-
-  it "shows challenge" do
-    get challenge_url(challenge)
-    value(response).must_be :success?
-  end
-
-  it "gets edit" do
-    get edit_challenge_url(challenge)
-    value(response).must_be :success?
-  end
-
-  it "updates challenge" do
-    patch challenge_url(challenge), params: { challenge: { category: challenge.category, description: challenge.description, name: challenge.name, num_points: challenge.num_points } }
-    must_redirect_to challenge_path(challenge)
-  end
-
-  it "destroys challenge" do
-    expect {
-      delete challenge_url(challenge)
-    }.must_change "Challenge.count", -1
-
-    must_redirect_to challenges_path
-  end
-end
-=======
 require "test_helper"
 
 class ChallengesControllerTest < ActionDispatch::IntegrationTest
@@ -108,4 +60,3 @@ class ChallengesControllerTest < ActionDispatch::IntegrationTest
   end
 
 end
->>>>>>> 7134f2a33a504e014c2751ded7fc17e4dd8ed54b
